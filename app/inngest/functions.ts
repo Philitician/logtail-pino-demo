d11df128153738f364cd56e7a030db61db640f28
Philip Rothschild <philiprene89@gmail.com>
2024-08-07 17:08:30 +0200

    Adding more logs

--- app/inngest/functions.ts
+++ app/inngest/functions.ts
@@ -4,9 +4,17 @@ export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step, logger }) => {
-    logger.info("Hello, World!");
+    logger.info("Incoming, running steps...");
+    await step.run("Step 1", () => {
+      logger.info("Step 1");
+    });
+    logger.info("Waiting for 1 second...");
     await step.sleep("wait-a-moment", "1s");
-    logger.info("Hello, World! (after 1s)");
+    logger.info("1 second has passed!");
+    await step.run("Step 2", () => {
+      logger.info("Step 2");
+    });
+    logger.info("Steps completed!");
     return { event, body: "Hello, World!" };
   }
 );
--- scripts/logger-test.ts
+++ scripts/logger-test.ts
@@ -1,4 +1,3 @@
 import { logger } from "@/lib/logger";
 
-// this is working!
 logger.info("Log from script");
