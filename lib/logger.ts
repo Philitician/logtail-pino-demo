eb373c06eec11c0653652577d10908af36b1b884
Philip Rothschild <philiprene89@gmail.com>
2024-08-07 18:10:17 +0200

    Bump

--- lib/logger.ts
+++ lib/logger.ts
@@ -5,10 +5,6 @@ const transport = pino.transport({
   target: "@logtail/pino",
   options: {
     sourceToken: process.env.LOGTAIL_SOURCE_TOKEN,
-    ignoreExceptions: false,
-    throwExceptions: true,
-    retryCount: 10,
-    sendLogsToConsoleOutput: true,
   },
 });
 export const logger = pino(transport);
